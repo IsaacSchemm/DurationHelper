7bd5ecc4186edf81f4b07a015f420a678de4d4e1
libertyernie <maybeway36@gmail.com>
2018-01-28 15:59:36 -0600

    Add more flexibility in GetDurationAsync

--- DurationHelper/MP4.cs
+++ DurationHelper/MP4.cs
@@ -10,12 +10,19 @@ namespace DurationHelper {
         public static async Task<TimeSpan?> GetDurationAsync(Stream stream) {
             if (stream == null) throw new ArgumentNullException();
             byte[] searchFor = Encoding.UTF8.GetBytes("mvhd");
-            byte[] tag = new byte[4];
+            byte[] b = new byte[1];
+            int lookingFor = 0;
             while (true) {
-                int r = await stream.ReadAsync(tag, 0, 4);
-                if (r < 4) break;
+                int r = await stream.ReadAsync(b, 0, 1);
+                if (r < 1) break;
 
-                if (tag.SequenceEqual(searchFor)) {
+                if (b[0] != searchFor[lookingFor]) {
+                    lookingFor = 0;
+                    continue;
+                }
+
+                lookingFor++;
+                if (lookingFor == searchFor.Length) {
                     for (int i = 0; i < 12; i++) stream.ReadByte();
 
                     uint timeScale = 0;
@@ -41,12 +48,12 @@ namespace DurationHelper {
             }
         }
 
-        public static async Task<TimeSpan?> GetDurationAsync(Uri url) {
+        public static async Task<TimeSpan?> GetDurationAsync(Uri url, int range = 256) {
             if (url == null) throw new ArgumentNullException();
 
             HttpWebRequest req = WebRequest.CreateHttp(url);
             req.UserAgent = Shared.UserAgent;
-            req.AddRange(0, 256);
+            req.AddRange(0, range);
             using (var resp = await req.GetResponseAsync()) {
                 using (var stream = resp.GetResponseStream()) {
                     return await GetDurationAsync(stream);
