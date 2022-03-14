5bed705543eb28c36b6266efe23b1d0afd32a181
Isaac Schemm <isaacschemm@gmail.com>
2022-03-13 21:54:41 -0500

    Use ISchemm.DurationFinder for almost everything

--- DurationHelper/Providers/Vimeo.cs
+++ DurationHelper/Providers/Vimeo.cs
@@ -1,38 +1,12 @@
-﻿using DurationHelper.Exceptions;
-using Newtonsoft.Json;
+﻿using ISchemm.DurationFinder;
 using System;
-using System.IO;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace DurationHelper.Providers {
+    [Obsolete]
     public static class Vimeo {
-        /// <summary>
-        /// Find the duration of a Vimeo video by its URL.
-        /// </summary>
-        /// <param name="url">The URL of the video</param>
-        /// <returns>The video duration</returns>
-        /// <exception cref="ArgumentNullException">url is null.</exception>
-        /// <exception cref="WebException">The Vimeo oEmbed request failed or returned a status outside of the 200 range.</exception>
-        /// <exception cref="JsonReaderException">The Vimeo oEmbed response could not be deserialized.</exception>
-        /// <exception cref="VideoNotFoundException">No video was found at the given URL.</exception>
         public static async Task<TimeSpan> GetDurationAsync(Uri url) {
-            if (url == null) throw new ArgumentNullException();
-
-            HttpWebRequest req = WebRequest.CreateHttp($"https://vimeo.com/api/oembed.json?url={WebUtility.UrlEncode(url.AbsoluteUri)}");
-            req.UserAgent = Shared.UserAgent;
-            try {
-                using (var resp = await req.GetResponseAsync()) {
-                    using (var sr = new StreamReader(resp.GetResponseStream())) {
-                        var obj = JsonConvert.DeserializeAnonymousType(await sr.ReadToEndAsync(), new {
-                            duration = 0.0
-                        });
-                        return TimeSpan.FromSeconds(obj.duration);
-                    }
-                }
-            } catch (WebException ex) when ((ex.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.NotFound) {
-                throw new VideoNotFoundException(ex);
-            }
+            return (await new OEmbedDurationProvider().GetDurationAsync(url)).Value;
         }
     }
 }
--- DurationHelper/Providers/YouTube.cs
+++ DurationHelper/Providers/YouTube.cs
@@ -1,100 +1,30 @@
-﻿using DurationHelper.Exceptions;
-using Newtonsoft.Json;
+﻿using ISchemm.DurationFinder;
 using System;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Xml;
 
 namespace DurationHelper.Providers {
     public class YouTube {
-        private string _key;
-
         private readonly static Regex REGEX_ID = new Regex(@"(?:(?:v|vi|be|videos|embed)/(?!videoseries)|(?:v|ci)=)([\w-]{11})", RegexOptions.IgnoreCase);
 
-        /// <summary>
-        /// Create a new DurationHelper.YouTube object with a YouTube Data API v3 key.
-        /// </summary>
-        /// <param name="key">The YouTube API key</param>
-        /// <exception cref="ArgumentNullException">key is null.</exception>
-        public YouTube(string key) {
-            _key = key ?? throw new ArgumentNullException();
-        }
+        public YouTube(string key) { }
 
-        /// <summary>
-        /// Extract a YouTube ID from a YouTube URL.
-        /// </summary>
-        /// <param name="url">The YouTube URL (youtube.com or youtu.be)</param>
-        /// <returns>The YouTube ID</returns>
-        /// <exception cref="VideoURLParseException">The URL format was not recognized as a YouTube URL.</exception>
         public static string GetIdFromUrl(Uri url) {
             var match = REGEX_ID.Match(url.AbsoluteUri);
-            return match.Success ? match.Groups[1].Value : throw new VideoURLParseException();
+            return match.Success ? match.Groups[1].Value : throw new FormatException("Could not extract video ID from URL");
         }
 
-        /// <summary>
-        /// Get the duration of a YouTube video by its ID.
-        /// </summary>
-        /// <param name="id">The video's YouTube ID</param>
-        /// <returns>The duration of the video</returns>
-        /// <exception cref="ArgumentNullException">id is null.</exception>
-        /// <exception cref="WebException">The YouTube API request failed.</exception>
-        /// <exception cref="JsonReaderException">The YouTube API response could not be deserialized.</exception>
-        /// <exception cref="FormatException">The duration could not be parsed from the YouTube API response.</exception>
-        /// <exception cref="YouTubeAPIException">A YouTube API error occurred.</exception>
-        /// <exception cref="TooManyRequestsException">The YouTube API quota has been exceeded.</exception>
-        /// <exception cref="VideoNotFoundException">No video was found at the given URL.</exception>
         public async Task<TimeSpan> GetDurationAsync(string id) {
             if (id == null) throw new ArgumentNullException();
 
-            HttpWebRequest req = WebRequest.CreateHttp($"https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id={WebUtility.UrlEncode(id)}&key={WebUtility.UrlEncode(_key)}");
-            req.UserAgent = Shared.UserAgent;
-            try {
-                using (var resp = await req.GetResponseAsync()) {
-                    using (var sr = new StreamReader(resp.GetResponseStream())) {
-                        string json = await sr.ReadToEndAsync();
-                        var obj = JsonConvert.DeserializeAnonymousType(json, new {
-                            items = new[] {
-                                new {
-                                    id = "",
-                                    contentDetails = new {
-                                        duration = ""
-                                    }
-                                }
-                            }
-                        });
-                        string ts = obj?.items?.Select(i => i.contentDetails.duration).FirstOrDefault();
-                        if (ts == null) {
-                            throw new VideoNotFoundException();
-                        } else {
-                            return XmlConvert.ToTimeSpan(ts);
-                        }
-                    }
-                }
-            } catch (WebException ex) when (ex.Response is HttpWebResponse r) {
-                throw await YouTubeAPIException.FromHttpWebResponseAsync(r);
-            }
+            return (await new SchemaOrgDurationProvider().GetDurationAsync(new Uri($"https://www.youtube.com/watch?v={Uri.EscapeDataString(id)}"))).Value;
         }
 
-        /// <summary>
-        /// Get the duration of a YouTube video by its URL.
-        /// </summary>
-        /// <param name="url">The video's YouTube URL</param>
-        /// <returns>The duration of the video</returns>
-        /// <exception cref="ArgumentNullException">url is null.</exception>
-        /// <exception cref="VideoURLParseException">The URL format was not recognized as a YouTube URL.</exception>
-        /// <exception cref="WebException">The YouTube API request failed.</exception>
-        /// <exception cref="JsonReaderException">The YouTube API response could not be deserialized.</exception>
-        /// <exception cref="FormatException">The duration could not be parsed from the YouTube API response.</exception>
-        /// <exception cref="YouTubeAPIException">A YouTube API error occurred.</exception>
-        /// <exception cref="TooManyRequestsException">The YouTube API quota has been exceeded.</exception>
-        /// <exception cref="VideoURLParseException">The URL format was not recognized as a YouTube URL.</exception>
+        [Obsolete]
         public async Task<TimeSpan?> GetDurationAsync(Uri url) {
             if (url == null) throw new ArgumentNullException();
-            
-            return await GetDurationAsync(GetIdFromUrl(url));
+
+            return await new SchemaOrgDurationProvider().GetDurationAsync(url);
         }
     }
 }
